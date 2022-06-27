72d3113dfdd6c9951a5554e5f05984d88b1d4b55
pdeguzman <padgzmn@gmail.com>
2022-06-27 18:40:08 -0400

    redo active record queries problems and completed queries 01

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -6,10 +6,9 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-
-  Movie
-      .select(:id, :title, :score)
-      .where("score BETWEEN 2 AND 3")
+    Movie 
+          .select(:id, :title, :score)
+          .where(score: 2..3)
 end
 
 def harrison_ford
@@ -23,12 +22,10 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-
-  Movie
-      .select(:title, :id)
-      .joins(:actors)
-      .where("name = 'Harrison Ford'")
-      .where.not(castings: {ord: 1})
+    Movie
+          .select(:id, :title)
+          .joins(:actors)
+          .where('castings.ord != 1 AND actors.name = (?)', 'Harrison Ford')
 end
 
 def biggest_cast
@@ -45,13 +42,13 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+    Movie
+          .select(:id, :title)
+          .joins(:actors)
+          .group(:id)
+          .order('COUNT(*) DESC')
+          .limit(3)
 
-  Movie
-      .select(:id, :title)
-      .joins(:castings)
-      .group('movie_id')
-      .order('COUNT(*) DESC')
-      .limit(3)
 end
 
 def directed_by_one_of(them)
@@ -66,11 +63,10 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
- 
-  Movie
-      .select(:id, :title)
-      .joins(:director)
-      .where('name IN (?)', them)
+    Movie
+          .select(:id, :title)
+          .joins(:director)
+          .where('actors.name IN (?)', them)
 
 end
 
@@ -85,7 +81,8 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+    Movie
+        .where('yr < 1940')
+        .pluck(:title)
 
-  Movie
-      .where('yr < 1940').pluck(:title)
 end
--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -3,18 +3,14 @@ def eighties_b_movies
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
 
-  Movie
-      .select(:id, :title, :yr, :score)
-      .where(yr: 1980..1989, score: 3..5)
+
 
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
 
-  Movie
-      .select(:yr)
-      .where.not("votes IS NULL")
+
 
 end
 
