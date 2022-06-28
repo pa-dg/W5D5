0e16f9c00aba67b8e14b2593d26c23ae874c196c
pdeguzman <padgzmn@gmail.com>
2022-06-27 20:36:38 -0400

    partial completion of queries 03

--- skeleton/movie_buff/03_queries.rb
+++ skeleton/movie_buff/03_queries.rb
@@ -1,11 +1,22 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+    Movie
+        .select(:id, :title)
+        .joins(:actors)
+        .where('actors.name IN (?)', those_actors)
+        .group(:id)
+        .having('COUNT(actors.id) = ?', those_actors.length)
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
+    Movie
+        .select('AVG(score), (yr / 10) * 10 AS decade')
+        .group('decade')
+        .order('avg(score) DESC')
+        .first
+        .decade
 
 end
 
